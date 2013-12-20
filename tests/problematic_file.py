9d71e56e35491dc2d3d5db967afcb414a8cddfbf
Marcin Kasperski <Marcin.Kasperski@mekk.waw.pl>
2013-12-20 02:32:22 +0100

    This still fails

--- tests/problematic_file.py
+++ tests/problematic_file.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
 
 """
-Tests for config file parsing and interpretation, plus proper default handling.
+Hello
 """
 
-from mekk.calibre.config import Config
+from a.b.c import Config
 
 def test_existing():
-
-    try:
-        cfg = Config(tf.name)
-    finally:
-        print tf.name
-        #os.remove(tf.name)
+    print name
